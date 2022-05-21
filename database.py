a560cd34870a7ddcb98a17d0eda703c5a58c8c53
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-05-21 20:10:52 +0100

    feature maladie 2

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ from path import PATH, MQ
 # load model
 model = load_model('brain-tumor-model.h5')
 classes = os.listdir(PATH+'Training')
-
+modelRetino = load_model('derma_diseases_detection.h5')
 
 def names(number):
     if(number == 0):
@@ -23,23 +23,60 @@ def names(number):
         return classes[2]
     elif(number == 3):
         return classes[3]
+    
+def retinas(number):
+    if(number == 0):
+        return 'Normal'
+    elif(number == 1):
+        return 'Mild'
+    elif(number == 2):
+        return 'Moderate'
+    elif(number == 3):
+        return 'Severe'
+    elif(number == 4):
+        return 'Proliferative'
+   
 
-
-def riddle(n):
+def riddleRetina(n):
+    oracle = ""
     # load image and verify if it's an image
     try:
         binary_data = io.BytesIO(n)
 
         img = Image.open(binary_data)
     except:
-        return "not an image"
+        oracle = "not an image"
 
-    x = np.array(img.resize((150, 150)))
-    x = x.reshape(1, 150, 150, 3)
-    answ = model.predict_on_batch(x)
-    classification = np.where(answ == np.amax(answ))[1][0]
-    oracle = str(answ[0][classification]*100) + \
-        '% Confidence This Is ' + names(classification)
+    try:
+        x = np.array(img.resize((150, 150)))
+        x = x.reshape(1, 150, 150, 3)
+        answ = modelRetino.predict_on_batch(x)
+        classification = np.where(answ == np.amax(answ))[1][0]
+        oracle = str(answ[0][classification]*100) + \
+            '% Confidence This Is ' + names(classification)
+    except:
+        oracle = "bad image"
+    return oracle
+
+def riddle(n):
+    oracle = ""
+    # load image and verify if it's an image
+    try:
+        binary_data = io.BytesIO(n)
+
+        img = Image.open(binary_data)
+    except:
+        oracle = "not an image"
+
+    try:
+        x = np.array(img.resize((150, 150)))
+        x = x.reshape(1, 150, 150, 3)
+        answ = model.predict_on_batch(x)
+        classification = np.where(answ == np.amax(answ))[1][0]
+        oracle = str(answ[0][classification]*100) + \
+            '% Confidence This Is ' + names(classification)
+    except:
+        oracle = "bad image"
     return oracle
 
 
@@ -48,10 +85,11 @@ ssl_options = pika.SSLOptions(context, MQ)
 connection = pika.BlockingConnection(pika.ConnectionParameters(
     ssl_options=ssl_options, port=5671, host=MQ, credentials=pika.PlainCredentials("kino", "1593578915935789")))
 channel = connection.channel()
-channel.queue_declare(queue='rpc_queue')
+channel.queue_declare(queue='rpc_brain')
+channel.queue_declare(queue='rpc_retino')
 
 
-def on_request(ch, method, props, body):
+def brain(ch, method, props, body):
 
     #  n = int.from_bytes(body, "big")
 
@@ -67,9 +105,24 @@ def on_request(ch, method, props, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
+def retino(ch, method, props, body):
+
+    print("classifying image...")
+    response = riddleRetina(body)
+    print(response)
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(
+                         correlation_id=props.correlation_id),
+                     body=str(response))
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
 def main():
     channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
+    channel.basic_consume(queue='rpc_brain', on_message_callback=brain)
+    channel.basic_consume(queue='rpc_retino', on_message_callback=retino)
     print(" [x] Awaiting RPC requests")
     channel.start_consuming()
     print(' [*] Waiting for messages. To exit press CTRL+C')
