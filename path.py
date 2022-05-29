c58d9c81ae30e12a5e26c907ffed674148f3fb62
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-05-29 13:48:22 +0100

    path

--- path.py
+++ path.py
@@ -1,2 +1,2 @@
-PATH ='/mnt/6822E49722E46C0E/Users/kadem/Desktop/Studies/sir/Jhip/keras_env/Brain-cancer/brain-tumor-classification-dataset/'
+PATH ='/home/Nyanpasu/Desktop/code/vscodegit/Brain-cancer/Brain-cancer/brain-tumor-classification-dataset/'
 MQ='b-709eef3a-fb78-408f-bf61-c4ce24bb71a5.mq.us-east-1.amazonaws.com'
